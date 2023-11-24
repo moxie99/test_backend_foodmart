35ef5af0a4e8d284e6bb0edb7d4e2c0fd250d2f4
Adeolu David <adeolusegun99@gmail.com>
2023-11-24 17:12:40 +0100

    added frontend to cors

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 
 app.use(
   cors({
-    origin: ['http://localhost:3001', 'http://localhost:3000', "https://foodmart-o1ja.onrender.com"],
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'https://foodmart-o1ja.onrender.com',
+      'https://foodmartuser.vercel.app/',
+    ],
     credentials: true,
   })
 );
