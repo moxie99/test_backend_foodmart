a990dff33db0b4cde8a9f0ddbfdfe7caa7db0959
Adeolu David <adeolusegun99@gmail.com>
2023-11-25 02:34:09 +0100

    token and cookies issues to solve

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async (req, res, next) => {
   const { accessToken } = req.cookies;
+  console.log(req);
   console.log('token ', accessToken);
   if (!accessToken) {
     return res.status(409).json({ error: 'Please login first' });
